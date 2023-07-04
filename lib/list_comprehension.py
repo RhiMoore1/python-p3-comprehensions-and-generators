86808b9d026aa905dd3aca2109fe37764f4de9cc
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-03 18:51:08 -0700

    completes lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+
+
+
+
+
+
+newList = []
 def return_evens(num_list):
-    pass
+    for n in num_list:
+        if n % 2 == 0:
+            newList.append(n)
+    return newList
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
