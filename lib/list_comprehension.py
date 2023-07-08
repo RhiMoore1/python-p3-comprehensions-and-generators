36c4bb8c5c13b024b249d9770cc35026e329cded
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-08 13:07:28 -0700

    updates lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
 
+def return_evens(number_list):
+    return[numb for numb in number_list if numb % 2 == 0]
+
+result = return_evens([1, 2, 3, 4, 5, 6, 7, 8])
+print(result)
 
 
-
-
-
-newList = []
-def return_evens(num_list):
-    for n in num_list:
-        if n % 2 == 0:
-            newList.append(n)
-    return newList
-
+# newList = []
+# def return_evens(num_list):
+#     for n in num_list:
+#         if n % 2 == 0:
+#             newList.append(n)
+#     return newList
 
 
 def make_exclamation(sentence_list):
